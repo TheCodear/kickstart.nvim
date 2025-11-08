b12410bab1e41f5709cc6ae448179dea903dfec0
Jan Wolfensberger <semerun33@gmail.com>
2025-11-08 21:51:24 +0100

    Add some LSP config

--- init.lua
+++ init.lua
@@ -508,6 +508,80 @@ require('lazy').setup({
     end,
   },
 
+  -- Plugins for Debugging
+  {
+    'leoluz/nvim-dap-go',
+    version = '*',
+    dependencies = {
+      'mfussenegger/nvim-dap',
+      config = function()
+        local map = function(keys, func, desc, mode)
+          mode = mode or 'n'
+          vim.keymap.set(mode, keys, func, { desc = 'DAP: ' .. desc })
+        end
+
+        map('<leader>dc', require('dap').continue, '[d]ap [c]ontinue -- launch debug session')
+        map('<leader>dn', require('dap').step_over, '[d]ap [n]ext / step over')
+        map('<leader>di', require('dap').step_into, '[d]ap [i]nto / step into')
+        map('<leader>dr', require('dap').repl.open, '[d]ap [r]epl open')
+        map('<leader>db', require('dap').toggle_breakpoint, '[d]ap toggle [b]reakpoint')
+      end,
+    },
+    opts = {
+      -- Additional dap configurations can be added.
+      -- dap_configurations accepts a list of tables where each entry
+      -- represents a dap configuration. For more details do:
+      -- :help dap-configuration
+      dap_configurations = {
+        {
+          -- Must be "go" or it will be ignored by the plugin
+          type = 'go',
+          name = 'Attach remote',
+          mode = 'remote',
+          request = 'attach',
+        },
+      },
+      -- delve configurations
+      delve = {
+        -- the path to the executable dlv which will be used for debugging.
+        -- by default, this is the "dlv" executable on your PATH.
+        path = 'dlv',
+        -- time to wait for delve to initialize the debug session.
+        -- default to 20 seconds
+        initialize_timeout_sec = 20,
+        -- a string that defines the port to start delve debugger.
+        -- default to string "${port}" which instructs nvim-dap
+        -- to start the process in a random available port.
+        -- if you set a port in your debug configuration, its value will be
+        -- assigned dynamically.
+        port = '${port}',
+        -- additional args to pass to dlv
+        args = {},
+        -- the build flags that are passed to delve.
+        -- defaults to empty string, but can be used to provide flags
+        -- such as "-tags=unit" to make sure the test suite is
+        -- compiled during debugging, for example.
+        -- passing build flags using args is ineffective, as those are
+        -- ignored by delve in dap mode.
+        -- avaliable ui interactive function to prompt for arguments get_arguments
+        build_flags = {},
+        -- whether the dlv process to be created detached or not. there is
+        -- an issue on delve versions < 1.24.0 for Windows where this needs to be
+        -- set to false, otherwise the dlv server creation will fail.
+        -- avaliable ui interactive function to prompt for build flags: get_build_flags
+        detached = vim.fn.has 'win32' == 0,
+        -- the current working directory to run dlv from, if other than
+        -- the current working directory.
+        cwd = nil,
+      },
+      -- options related to running closest test
+      tests = {
+        -- enables verbosity when running the test.
+        verbose = false,
+      },
+    },
+  },
+
   -- LSP Plugins
   {
     -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
@@ -719,8 +793,17 @@ require('lazy').setup({
       local servers = {
         texlab = {},
         gopls = {},
-        -- clangd = {},
-        -- pyright = {},
+
+        -- This is used for elixir code
+        expert = {
+          cmd = { 'expert' },
+          filetypes = { 'elixir', 'eelixir', 'heex' },
+          settings = {
+            root_markers = { 'mix.exs', '.git' },
+          },
+        },
+        clangd = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
@@ -763,7 +846,6 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'gopls',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
